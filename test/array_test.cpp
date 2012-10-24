da2842a7eea54d9af02b110fac9a061b5693e0ec
Zack Michener <zack@zackmichener.com>
2012-10-24 19:39:58 -0400

    removed extraneous tests

--- test/array_test.cpp
+++ test/array_test.cpp
@@ -6,10 +6,6 @@ using std::cout;
 using std::endl;
 using std::string;
 
-TEST(ProjectTest, CompileTest) {
-
-}
-
 TEST(ArrayTest, Constructor) {
     TwoDArray<int>* ia1 = new TwoDArray<int>(1,1,0);
     delete ia1;
@@ -25,17 +21,3 @@ TEST(ArrayTest, Constructor) {
 
 }
 
-TEST(ArrayTest, print) {
- //   TwoDArray<int>* ia = new TwoDArray<int>(4, 4, 0);
-    //string expected = "0 0 0 0\n0 0 0 0\n0 0 0 0\n0 0 0 0";
-}
-
-TEST(ArrayTest, insert) {
-//    TwoDArray<int>* ia = new TwoDArray<int>(10,10, 0);
-
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            //ia->insert(i,j,i*j);
-        }
-    }
-}
